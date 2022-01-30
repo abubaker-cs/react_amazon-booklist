9d14faa50a00805649400624d153e0e0f314ac9c
abubaker-cs <mohammad_abubaker@yahoo.com>
2022-01-31 03:26:35 +0500

    Understanding props

--- src/index.js
+++ src/index.js
@@ -3,91 +3,90 @@ import React from "react";
 // It will render the main component
 import ReactDom from "react-dom";
 
-// Add Foundation to index.js
-// import "foundation-sites/dist/css/foundation.min.css";
-
-// Import react-foundation components
-import {
-  Grid,
-  Cell,
-  Menu,
-  MenuItem,
-  MediaObject,
-  MediaObjectSection,
-  Thumbnail,
-} from "react-foundation";
-
 // Connecting the index.css file
 import "./index.css";
+
+// Setup
+const firstBook = {
+  img: "https://images-na.ssl-images-amazon.com/images/I/51G2Y+mbUpL._AC_UL604_SR604,400_.jpg",
+  title: "The Last Thing He Told Me: A Novel",
+  author: "Laura Dave",
+  bookMaterial: "Paperback",
+  price: "$19.99",
+};
+
+const secondBook = {
+  img: "https://images-na.ssl-images-amazon.com/images/I/51p2SDOCV9L._SX482_BO1,204,203,200_.jpg",
+  title: "I Love You to the Moon and Back",
+  author: "Amelia Hepworth",
+  bookMaterial: "Paperback",
+  price: "$13.99",
+};
+
+const thirdBook = {
+  img: "https://images-na.ssl-images-amazon.com/images/I/41A+2VWGZwS._SX331_BO1,204,203,200_.jpg",
+  title: "Enough Already: Learning to Love the Way I Am Today",
+  author: "Valerie Bertinelli",
+  bookMaterial: "Hardcover ",
+  price: "$14.99",
+};
+
+const fourthBook = {
+  img: "https://images-na.ssl-images-amazon.com/images/I/51YZglhcfpL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg",
+  title: "Redeeming Love",
+  author: "Francine Rivers",
+  bookMaterial: "Paperback  ",
+  price: "$11.99",
+};
 
 // BookList
 function BookList() {
   return (
     <section className="bookList">
-      <Book />
+      <Book
+        img={fourthBook.img}
+        title={fourthBook.title}
+        author={fourthBook.author}
+        bookMaterial={fourthBook.bookMaterial}
+        price={fourthBook.price}
+      />
+      <Book
+        img={thirdBook.img}
+        title={thirdBook.title}
+        author={thirdBook.author}
+        bookMaterial={thirdBook.bookMaterial}
+        price={thirdBook.price}
+      />
+
+      <Book
+        img={secondBook.img}
+        title={secondBook.title}
+        author={secondBook.author}
+        bookMaterial={secondBook.bookMaterial}
+        price={secondBook.price}
+      />
+
+      <Book
+        img={firstBook.img}
+        title={firstBook.title}
+        author={firstBook.author}
+        bookMaterial={firstBook.bookMaterial}
+        price={firstBook.price}
+      />
     </section>
   );
 }
 
 // Single Book
-const Book = () => {
-  const imageURL =
-    "https://images-na.ssl-images-amazon.com/images/I/51G2Y+mbUpL._AC_UL604_SR604,400_.jpg";
-  const title = "The Last Thing He Told Me: A Novel";
-  const author = "Laura Dave";
-  const bookMaterial = "Paperback";
-  const price = "$19.99";
-
+const Book = (props) => {
   return (
     <article className="singleBook">
-      <img src={imageURL} alt="placeholder" className="thumbnail" />
-      <h1>{title}</h1>
-      <p className="bookAuthor">by {author}</p>
-      <p>{bookMaterial}</p>
-      <p>{price}</p>
+      <img src={props.img} alt="placeholder" className="thumbnail" />
+      <h1>{props.title}</h1>
+      <p className="bookAuthor">by {props.author}</p>
+      <p>{props.bookMaterial}</p>
+      <p>{props.price}</p>
     </article>
-  );
-};
-
-// ------------------------------------------------------------
-// Zurb Header
-const Header = () => {
-  return (
-    <Menu>
-      <MenuItem>
-        <a href="/">Home</a>
-      </MenuItem>
-      <MenuItem>
-        <a href="/">Blog</a>
-      </MenuItem>
-      <MenuItem>
-        <a href="/">About</a>
-      </MenuItem>
-      <MenuItem>
-        <a href="/">Contact</a>
-      </MenuItem>
-    </Menu>
-  );
-};
-
-// Zurb Card
-const Card = () => {
-  return (
-    <div className="cardlayout">
-      <MediaObject>
-        <MediaObjectSection>
-          <Thumbnail src="https://source.unsplash.com/random/100x100" />{" "}
-        </MediaObjectSection>
-        <MediaObjectSection isMain>
-          <h4>Important Heading Here</h4>
-          <p>
-            {" "}
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
-            mollitia, molestiae quas vel sint commod.
-          </p>
-        </MediaObjectSection>
-      </MediaObject>
-    </div>
   );
 };
 
---
+++ src/zurb.js
@@ -0,0 +1,93 @@
+import React from "react";
+
+// It will render the main component
+import ReactDom from "react-dom";
+
+// Add Foundation to index.js
+// import "foundation-sites/dist/css/foundation.min.css";
+
+// Import react-foundation components
+import {
+  Grid,
+  Cell,
+  Menu,
+  MenuItem,
+  MediaObject,
+  MediaObjectSection,
+  Thumbnail,
+} from "react-foundation";
+
+// Connecting the index.css file
+import "./index.css";
+
+// BookList
+function BookList() {
+  return (
+    <section className="bookList">
+      <Book />
+    </section>
+  );
+}
+
+// Single Book
+const Book = () => {
+  const imageURL =
+    "https://images-na.ssl-images-amazon.com/images/I/51G2Y+mbUpL._AC_UL604_SR604,400_.jpg";
+  const title = "The Last Thing He Told Me: A Novel";
+  const author = "Laura Dave";
+  const bookMaterial = "Paperback";
+  const price = "$19.99";
+
+  return (
+    <article className="singleBook">
+      <img src={imageURL} alt="placeholder" className="thumbnail" />
+      <h1>{title}</h1>
+      <p className="bookAuthor">by {author.toUpperCase()}</p>
+      <p>{bookMaterial}</p>
+      <p>{price}</p>
+    </article>
+  );
+};
+
+// Zurb Header
+const Header = () => {
+  return (
+    <Menu>
+      <MenuItem>
+        <a href="/">Home</a>
+      </MenuItem>
+      <MenuItem>
+        <a href="/">Blog</a>
+      </MenuItem>
+      <MenuItem>
+        <a href="/">About</a>
+      </MenuItem>
+      <MenuItem>
+        <a href="/">Contact</a>
+      </MenuItem>
+    </Menu>
+  );
+};
+
+// Zurb Card
+const Card = () => {
+  return (
+    <div className="cardlayout">
+      <MediaObject>
+        <MediaObjectSection>
+          <Thumbnail src="https://source.unsplash.com/random/100x100" />{" "}
+        </MediaObjectSection>
+        <MediaObjectSection isMain>
+          <h4>Important Heading Here</h4>
+          <p>
+            {" "}
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
+            mollitia, molestiae quas vel sint commod.
+          </p>
+        </MediaObjectSection>
+      </MediaObject>
+    </div>
+  );
+};
+
+ReactDom.render(<BookList />, document.getElementById("root"));
